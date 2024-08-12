48dc135fd533997d2b50e07309581d976a0d1b9a
raquel-cmps <raquelcampos@edu.unifil.br>
2024-08-12 20:15:27 -0300

    model e migrations

---
+++ Config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "root",
+    database: "api_node",
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}

bbb3aa7b9a99fc5a3ae485bcda317cb016853dd5
raquel-cmps <raquelcampos@edu.unifil.br>
2024-08-16 21:49:21 -0300

    database e populando migration

--- migrations/20240812231049-create-endereco.js
+++ migrations/20240812231049-create-endereco.js
@@ -2,21 +2,46 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('enderecos', {
+      Id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      Cep: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Logradouro: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Complemento: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Bairro: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Cidade: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Estado: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('enderecos');
   }
 };

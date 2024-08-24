79ef7fb9b4b4855fdd274d7746bfe18b02e8f3a7
raquel-cmps <raquelcampos@edu.unifil.br>
2024-08-23 21:00:47 -0300

    controller de cep

--- controller/EnderecoController.js
+++ controller/EnderecoController.js
@@ -92,3 +92,71 @@ exports.deleteEndereco = async (req, res) => {
         res.status(500).json({ error: 'Erro ao deletar endereço', details: error.message });
     }
 };
+
+exports.recebeCep = async (req, res) => {
+    try {
+        const { cep } = req.params; // Obtém o CEP da URL
+
+        // Chama a função de consulta do CEP
+        const endereco = await consultaCep(cep);
+
+        // Desestrutura as informações do endereço
+        const {
+            cep: Cep,
+            logradouro: Logradouro,
+            complemento: Complemento,
+            bairro: Bairro,
+            localidade: Cidade,
+            uf: Estado,
+            ibge: MunicipioIBGE
+        } = endereco;
+
+        // Cria um novo endereço no banco de dados
+        const novoEndereco = await Endereco.create({
+            Cep,
+            Logradouro,
+            Numero: 130,  // Certifique-se de que o número está sendo enviado no corpo da requisição
+            Complemento,
+            Bairro,
+            Cidade,
+            Estado,
+            MunicipioIBGE
+        });
+
+        // Retorna o novo endereço criado
+        res.status(201).json(novoEndereco);
+    } catch (error) {
+        // Loga o erro no console
+        console.error('Erro ao criar o endereço', error);
+
+        // Retorna uma resposta de erro ao cliente
+        res.status(500).json({ error: 'Erro ao criar o endereço', details: error.message });
+    }
+};
+
+
+exports.buscarCep = async (req, res) => {
+    const cep = req.params.cep;
+
+    try {
+        const data = await consultaCep(cep); // Chama a função consultaCep
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error making the request', error);
+        res.status(500).send(error.message);
+    }
+};
+
+// Função para validar e consultar o CEP
+const axios = require('axios');
+async function consultaCep(cep) {
+    const cepRegex = /^[0-9]{5}-?[0-9]{3}$/; // Regex para validar o CEP
+
+    let valid = cepRegex.test(cep);
+    if (!valid) {
+        throw new Error('Invalid CEP');
+    }
+
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    return response.data;
+}
--- routes.js
+++ routes.js
@@ -9,4 +9,6 @@ router.get('/enderecos/:Id', enderecoController.getEnderecoById);
 router.put('/enderecos/:Id', enderecoController.updateEndereco);
 router.delete('/enderecos/:Id', enderecoController.deleteEndereco);
 
+router.get('/buscarCep/:cep', enderecoController.recebeCep);
+router.get('/consulta-cep/:cep', enderecoController.buscarCep);
 module.exports = router;
